232e848b9189ff31752f033e685cf2b805e3833d
shinsaku-sasaki <shinsakusasakiengineer39@gmail.com>
2025-02-25 21:10:16 +0900

    TimeDisplayISRおよびTimeDisplaySSRコンポーネントから不要な遅延処理を削除

--- src/components/TimeDisplaySSR.tsx
+++ src/components/TimeDisplaySSR.tsx
@@ -1,7 +1,6 @@
 import ShowTime from "./ShowTime";
 
 async function getTimeData() {
-    await new Promise(resolve => setTimeout(resolve, 1000));
     const res = await fetch(`https://www.timeapi.io/api/Time/current/zone?timeZone=Asia/Tokyo`, {
         cache: 'no-store'
     });
